0c779816a882139c6573295e3a983e2f4cee9609
princenandoliya <princenandoliya@gmail.com>
2025-10-07 16:23:44 +0530

    Add files via upload

---
+++ c language/c-4/conts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define pi 3.14
+
+int main()
+{
+	
+	printf("%f ",pi);
+	
+	
+	
+	
+	
+	const int num =10;
+	printf("\n%d ",num);
+
+	return 0; 
+	
+}
---
+++ c language/c-4/form.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+int main()
+{
+	 int  num = 10;
+	 
+	 float type = 10.555;
+	 printf("%d",num);
+	 printf("\n%3d",num);
+	 
+	 
+	 printf("\n%f",type);
+	 printf("%4.f\n",type);
+	 return 0;
+}
