27571477837e2256814d3f705a340a723351ebf0
princenandoliya <princenandoliya@gmail.com>
2025-10-09 15:45:39 +0530

    Add files via upload

---
+++ c language/operator/operator-1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 20;
+	
+	int b = 70;
+	
+	
+	//addition
+	
+	printf("addition :%d\n",a+b);
+	
+	
+	// substraction
+	printf("substraction :%d\n",a-b);
+	
+	
+	
+	//multiplication
+	
+	printf("multiplication :%d\n",a*b);
+	
+	//division
+	
+	printf("division :%d\n",a / b);
+	
+	
+	//modulo
+	
+	printf("modulo :%d",a%b);
+}
