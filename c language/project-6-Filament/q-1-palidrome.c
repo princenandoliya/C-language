587ca9bd66bdbf0f7490f6171925f694f54534fa
princenandoliya <princenandoliya@gmail.com>
2025-12-11 16:07:27 +0530

    Add files via upload

---
+++ c language/project-6-Filament/q-1-palidrome.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+int main()
+{
+	char str[100], rev[200];
+	int i,j, len =0;
+	
+	printf("enter any string\n");
+	scanf("%S",str);
+	
+	for(i=0; str[i]!='\0' ;i++){
+		len++;
+	}
+	
+	j = len-1;
+	for(i=0;i<len;i++){
+		rev[i]=str[j];
+		j--;
+	}
+	
+	rev[i]='\0';
+	
+	int palindrome =1;
+	for( i=0;i<len;i++){
+		if(str[i] != rev[i])
+		{
+			palindrome=0;
+			break;
+		}
+	}
+	
+	if(palindrome ==1){
+		printf("output: the given string is a palindrome.");		
+	}
+	
+	else{
+			printf("output: the given string is  not a palindrome");
+	}
+
+
+	
+	
+	return 0;
+}
+
+
+
+
+// int main()
+// {
+// 	char str[100], rev[100];  
+// 	int i, j, len = 0;
+	
+// 	// Prompt for input
+// 	printf("Enter any string: ");
+// 	scanf("%s", str);  
+	
+// 	for (i = 0; str[i] != '\0'; i++) {
+// 		len++;
+// 	}
+	
+// 	j = len - 1;
+// 	for (i = 0; i < len; i++) {
+// 		rev[i] = str[j];
+// 		j--;
+// 	}
+	
+// 	rev[i] = '\0';  
+	
+// 	int palindrome = 1;  
+// 	for (i = 0; i < len; i++) {
+// 		if (str[i] != rev[i]) {
+// 			palindrome = 0;  
+// 			break;
+// 		}
+// 	}
+	
+	
+// 	if (palindrome == 1) {
+// 		printf("Output: The given string is a palindrome.\n");
+// 	} else {
+// 		printf("Output: The given string is not a palindrome.\n");
+// 	}
+
+// 	return 0; 
+// }
